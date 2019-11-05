85c886e77751d085fbaf136fa98215c790010adb
David Lodi <lodi.dev@outlook.com>
2019-11-05 00:06:55 -0600

    base con un nav

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Aqui van las variables
+
+  int currentIndex = 0;
+  
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hol'),
-      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigatorBar(),
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _crearBottomNavigatorBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+         currentIndex = index; 
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Maps')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Dirreciones'))
+      ],
     );
   }
 }
