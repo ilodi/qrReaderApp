2688355a3152ba0b5d6e0673bf2f9ee6be39c324
David Lodi <lodi.dev@outlook.com>
2019-11-07 00:05:09 -0600

    add to pkg qg y path

--- lib/src/models/scan_model.dart
+++ lib/src/models/scan_model.dart
@@ -1,23 +1,29 @@
 class ScanModel {
-    int id;
-    String tipo;
-    String valor;
+  int id;
+  String tipo;
+  String valor;
 
-    ScanModel({
-        this.id,
-        this.tipo,
-        this.valor,
-    });
+  ScanModel({
+    this.id,
+    this.tipo,
+    this.valor,
+  }) {
+    if (this.valor.contains('http')) {
+      this.tipo = 'http';
+    } else {
+      this.tipo = 'geo';
+    }
+  }
 //crea una nueva instancia
-    factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
         id: json["id"],
         tipo: json["tipo"],
         valor: json["valor"],
-    );
+      );
 //toJson retorna un objeto del mismo tipo
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "tipo": tipo,
         "valor": valor,
-    };
+      };
 }
