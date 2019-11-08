180bb8a125f9a4a73b0d3844552173b7ab93eb36
David Lodi <lodi.dev@outlook.com>
2019-11-07 20:46:27 -0600

    crear registro

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -33,9 +34,29 @@ class DBProvider {
         await db.execute('CREATE TABLE Scans('
             ' id INTEGER PRIMARY KEY,  '
             ' tipo TEXT, '
-            'valor TEXT '
+            ' valor TEXT '
             ')');
       },
     );
+  }
+
+  /// CREAR REGISTROS DE DB
+//MODO A
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.rawInsert(
+        //El espacio es importante
+         "INSERT Into Scans (id, tipo, valor) "
+        "VALUES ( ${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}' )");
+    return res;
+  }
+
+//MODO B
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = db.insert('Scans', nuevoScan.toJson());
+    return res;
   }
 }
