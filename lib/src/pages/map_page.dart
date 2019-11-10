fac2a3e1c528cb65de47ddd463de5c831f000575
David Lodi <lodi.dev@outlook.com>
2019-11-10 13:12:57 -0600

    el token tenia un detalle se uso el del tutorial

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:latlong/latlong.dart';
-import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:flutter_map/flutter_map.dart';
-
+//import 'package:latlong/latlong.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 
 class MapaPage extends StatelessWidget {
   @override
@@ -12,40 +11,39 @@ class MapaPage extends StatelessWidget {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cordenadas QR'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.my_location),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: _crearFLutterMap(scan),
-    );
+        appBar: AppBar(
+          title: Text('Cordenadas QR'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: _crearFlutterMap(scan));
   }
 
-  Widget _crearFLutterMap(ScanModel scan) {
-    return new FlutterMap(
-      options: MapOptions(center: scan.getLatLng(),
-      zoom: 10,
-  
-      ),
-  layers: [
-    _crearMapa(),
-  ],
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(center: scan.getLatLng(), zoom: 13.0),
+      layers: [
+        _crearMapa(),
+      ],
     );
   }
 
   _crearMapa() {
+
     return TileLayerOptions(
-       urlTemplate: 'https://api.mapbox.com/v4/'
+      urlTemplate: 'https://api.mapbox.com/v4/'
       '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
-        'accessToken': 'pk.eyJ1IjoiaWxvZGliaXRzIiwiYSI6ImNrMnNpdXFpYzBqcDMzbW54eWJhNDJ0aXkifQ.w82msNeduaUYHKEsCSfE0Q',
-        'id': 'mapbox.Streets',
+        'accessToken': 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJjanY2MjF4NGIwMG9nM3lvMnN3ZDM1dWE5In0.0SfmUpbW6UFj7ZnRdRyNAw',
+        'id': 'mapbox.streets'
       }
-
     );
+
+
   }
+
 }
