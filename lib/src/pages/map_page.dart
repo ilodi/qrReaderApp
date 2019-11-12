2c2fd3b2f24690a7ce72ba2e1bf24036d48914df
David Lodi <lodi.dev@outlook.com>
2019-11-11 21:59:46 -0600

    changes map styles

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -3,7 +3,15 @@ import 'package:flutter_map/flutter_map.dart';
 //import 'package:latlong/latlong.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  MapController map = new MapController();
+  String tipomapa = 'streets';
+
   @override
   Widget build(BuildContext context) {
     // Extrae los argumentos de la propiedad settings del ModalRoute actual y lo convierte
@@ -11,20 +19,47 @@ class MapaPage extends StatelessWidget {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cordenadas QR'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.my_location),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: _crearFlutterMap(scan));
+      appBar: AppBar(
+        title: Text('Cordenadas QR'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.my_location),
+            onPressed: () {
+              map.move(scan.getLatLng(), 15.0);
+            },
+          )
+        ],
+      ),
+      body: _crearFlutterMap(scan),
+      floatingActionButton: _crearBotonFlotante(context),
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        setState(() {
+          if (tipomapa == 'streets') {
+            tipomapa = 'dark';
+          } else if (tipomapa == 'dark') {
+            tipomapa = 'light';
+          } else if (tipomapa == 'light') {
+            tipomapa = 'outdoors';
+          } else if (tipomapa == 'outdoors') {
+            tipomapa = 'satellite';
+          } else {
+            tipomapa = 'streets';
+          }
+        });
+      },
+    );
   }
 
   Widget _crearFlutterMap(ScanModel scan) {
     return FlutterMap(
+      mapController: map,
       options: MapOptions(center: scan.getLatLng(), zoom: 15.0),
       layers: [
         _crearMapa(),
@@ -34,33 +69,32 @@ class MapaPage extends StatelessWidget {
   }
 
   _crearMapa() {
-
     return TileLayerOptions(
       urlTemplate: 'https://api.mapbox.com/v4/'
-      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+          '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
-        'accessToken': 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJjanY2MjF4NGIwMG9nM3lvMnN3ZDM1dWE5In0.0SfmUpbW6UFj7ZnRdRyNAw',
-        'id': 'mapbox.dark'
+        'accessToken':
+            'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJjanY2MjF4NGIwMG9nM3lvMnN3ZDM1dWE5In0.0SfmUpbW6UFj7ZnRdRyNAw',
+        'id': 'mapbox.$tipomapa'
         //TIPOS DE MAPAs
         // streets, dark, light, outdoors, satellite
       },
     );
-
   }
 
-  _crearMarcadores(ScanModel scan){
-  return MarkerLayerOptions(
-    markers: <Marker>[
+  _crearMarcadores(ScanModel scan) {
+    return MarkerLayerOptions(markers: <Marker>[
       Marker(
-        width: 100.0,
-        height: 100.0,
-        point: scan.getLatLng(),
-        builder: (context) => Container(
-          child: Icon(Icons.location_on, size: 55.0, color: Theme.of(context).primaryColor,),
-        )
-      ),
-    ]
-  );
-}
-
+          width: 100.0,
+          height: 100.0,
+          point: scan.getLatLng(),
+          builder: (context) => Container(
+                child: Icon(
+                  Icons.location_on,
+                  size: 55.0,
+                  color: Theme.of(context).primaryColor,
+                ),
+              )),
+    ]);
+  }
 }
